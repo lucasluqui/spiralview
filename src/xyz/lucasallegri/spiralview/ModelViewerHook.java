0f4ed01a0721ce32d152b5eb485f55bd6554b21c
Lucas Allegri <lucasallegri@protonmail.com>
2020-02-22 18:09:13 -0300

    This one too

--- src/xyz/lucasallegri/spiralview/ModelViewerHook.java
+++ src/xyz/lucasallegri/spiralview/ModelViewerHook.java
@@ -15,14 +15,14 @@ public class ModelViewerHook extends ModelViewer {
 	
 	public ModelViewerHook(String model) {
 		super(model);
-	    JMenuBar mbar = this._frame.getJMenuBar();
-	    JMenu file = mbar.getMenu(0);
-	    JMenu environment = new JMenu("Environment");
-	    environment.add(new JMenuItem(new SavePrefsAction()));
-	    environment.add(new JMenuItem(new RestorePrefsAction()));
-	    environment.add(new JMenuItem(new ClearPrefsAction()));
-	    file.add(new JMenuItem(new SnapshotAction(this)), 0);
-	    file.add(environment, 0);
+		JMenuBar mbar = this._frame.getJMenuBar();
+		JMenu file = mbar.getMenu(0);
+		JMenu environment = new JMenu("Environment");
+		environment.add(new JMenuItem(new SavePrefsAction()));
+		environment.add(new JMenuItem(new RestorePrefsAction()));
+		environment.add(new JMenuItem(new ClearPrefsAction()));
+		file.add(new JMenuItem(new SnapshotAction(this)), 0);
+		file.add(environment, 0);
 	}
 	
 	public static void main(String[] args) {
