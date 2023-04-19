40ccea521c76d2401513a90da4ff305fb7aab382
Lucas Allegri <lucasallegri@protonmail.com>
2023-04-19 10:46:42 -0300

    Don't attempt to pass any init files

--- src/main/java/net/lucasallegri/spiralview/ParticleEditorHook.java
+++ src/main/java/net/lucasallegri/spiralview/ParticleEditorHook.java
@@ -5,7 +5,7 @@ import com.threerings.opengl.effect.tools.ParticleEditor;
 public class ParticleEditorHook {
 
   public static void main(String[] args) {
-    new ParticleEditor(args.length > 0 ? args[0] : null).startup();
+    new ParticleEditor(null).startup();
   }
 
 }
--- src/main/java/net/lucasallegri/spiralview/SceneEditorHook.java
+++ src/main/java/net/lucasallegri/spiralview/SceneEditorHook.java
@@ -5,7 +5,7 @@ import com.threerings.tudey.tools.SceneEditor;
 public class SceneEditorHook {
 
   public static void main(String[] args) {
-    new SceneEditor(args.length > 0 ? args[0] : null).startup();
+    new SceneEditor(null).startup();
   }
 
 }
