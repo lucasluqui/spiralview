82da4717555067b6bf09eff8c36f9a7b05fd121e
Lucas Allegri <lucasallegri@protonmail.com>
2024-08-07 16:41:20 -0300

    Up version to 2.0

--- src/main/java/com/luuqui/spiralview/Globals.java
+++ src/main/java/com/luuqui/spiralview/Globals.java
@@ -2,7 +2,7 @@ package com.luuqui.spiralview;
 
 public class Globals {
 
-  public static final String VERSION = "1.8.5";
+  public static final String VERSION = "2.0";
   public static final String USER_DIR = System.getProperty("user.dir");
 
 }
