7db43216f41b2e896f6f74718caecfbf05711f58
Lucas <lucasallegri@protonmail.com>
2024-11-12 19:42:58 -0300

    Bump version to 2.0.5

--- src/main/java/com/luuqui/spiralview/Globals.java
+++ src/main/java/com/luuqui/spiralview/Globals.java
@@ -2,7 +2,7 @@ package com.luuqui.spiralview;
 
 public class Globals {
 
-  public static final String VERSION = "2.0.4";
+  public static final String VERSION = "2.0.5";
   public static final String USER_DIR = System.getProperty("user.dir");
 
 }
