7b063ce691b5e70866a58e4736e80ce0fd2d835e
Lucas <lucasallegri@protonmail.com>
2026-01-01 17:40:04 -0300

    Obfuscation fixes.

--- src/main/java/com/threerings/projectx/config/ObjectiveConfig.java
+++ src/main/java/com/threerings/projectx/config/ObjectiveConfig.java
@@ -1,87 +1,56 @@
 package com.threerings.projectx.config;
 
-// add Simple empty constructor
-// replace return type of b method
-// fix return type of b method within simple class
-
 import com.threerings.config.ConfigReference;
-import com.threerings.editor.c;
-import com.threerings.editor.e;
-import com.threerings.export.Exportable;
-import com.threerings.export.f;
-import com.threerings.io.Streamable;
-import com.threerings.opengl.gui.ax;
-import com.threerings.opengl.gui.p;
+import com.threerings.opengl.gui.ay;
 import com.threerings.opengl.gui.config.UserInterfaceConfig;
-import com.threerings.projectx.client.er;
+import com.threerings.projectx.client.ew;
 import com.threerings.projectx.uplink.data.SystemSender;
-import com.threerings.projectx.util.A;
+import com.threerings.projectx.util.C;
 import com.threerings.tudey.config.ActionConfig;
-import com.threerings.util.g;
 
-@e({ObjectiveConfig.Simple.class, ObjectiveConfig.ReadSystemMail.class})
-public abstract class ObjectiveConfig extends g implements f, Streamable {
+public class ObjectiveConfig
+{
   public String getText() {
     return null;
   }
-
-  public ax b(A var1, er var2) {
+  public Object b (C var1, ew var2) {
     return null;
   }
 
-  public static class Simple extends ObjectiveConfig implements Exportable {
-    @c
-    public String text = "";
-    @c(
-        iM = 0.01,
-        iC = "c"
-    )
-    public float childX = 0.5F;
-    @c(
-        iM = 0.01,
-        iC = "c"
-    )
-    public float childY = 0.5F;
-    @c(
-        iM = 0.01,
-        iC = "p"
-    )
-    public float parentX = 0.5F;
-    @c(
-        iM = 0.01,
-        iC = "p"
-    )
-    public float parentY = 0.5F;
-    @c(
-        iR = true
-    )
-    public ConfigReference<UserInterfaceConfig> userInterface;
+  // does this work?
+  public Object b (C var1, Object var2) {
+    return null;
+  }
 
-    public Simple() {
-    }
+  public static class Simple
+  {
+    public Simple () {}
+
+    public String text = "";
+    public ConfigReference<UserInterfaceConfig> userInterface;
 
     public final String getText() {
       return this.text;
     }
 
-    public final ax b(A var1, er var2) {
+    public final Object b (C var1, ew var2)
+    {
       if (this.userInterface == null) {
         return null;
       } else {
-        ax var3 = new ax(var1, this.userInterface);
-        //var2.xK().add(var3, new b.a(this.childX, this.childY, this.parentX, this.parentY, 0, 0, false));
+        Object var3 = new ay(var1, this.userInterface);
         return var3;
       }
     }
   }
 
-  public static class ReadSystemMail extends Simple {
-    @c
+  public static class ReadSystemMail extends ObjectiveConfig.Simple
+  {
     public SystemSender sender;
-    @c
     public ActionConfig action;
 
-    public ReadSystemMail() {
+    public ReadSystemMail ()
+    {
       this.sender = SystemSender.ORIENTATION;
       this.action = new ActionConfig.SpawnActor();
     }
--- src/main/java/com/threerings/projectx/data/Achievement.java
+++ src/main/java/com/threerings/projectx/data/Achievement.java
@@ -1,46 +1,19 @@
 package com.threerings.projectx.data;
 
-import com.samskivert.util.z;
-import com.threerings.projectx.item.data.ItemCodes;
-import com.threerings.util.L;
-
-// set method b to return 0 instead of all the gibberish.
-// comment super() call in Achievement constructor.
-// set all qualifies to false.
-
-public class Achievement {
-  protected long _unlocked;
-  private static z<Type> ave;
-
-  public static int b(Type var0) {
+public class Achievement
+{
+  public static int b (Type var0) {
     return 0;
   }
 
-  public Achievement(Type var1, long var2) {
-    //super(var1);
-    this._unlocked = var2;
-  }
-
-  public static enum Type {
-    REACHED_RESCUE_CAMP {
-      public final boolean qualifies(PlayerObject var1) {
-        return false;
-      }
-    },
+  public enum Type {
+    REACHED_RESCUE_CAMP,
     REACHED_HAVEN,
     REACHED_TERMINAL_1,
     REACHED_SUBTOWN_1,
-    EARNED_TIER_2 {
-      public final boolean qualifies(PlayerObject var1) {
-        return false;
-      }
-    },
+    EARNED_TIER_2,
     REACHED_SUBTOWN_2,
-    EARNED_TIER_3 {
-      public final boolean qualifies(PlayerObject var1) {
-        return false;
-      }
-    },
+    EARNED_TIER_3,
     REACHED_CORE_TERMINAL,
     DEFEATED_ROYAL_JELLY,
     DEFEATED_VANADUKE,
@@ -51,121 +24,29 @@ public class Achievement {
     CRAFTED_RARITY_3,
     CRAFTED_RARITY_4,
     CRAFTED_RARITY_5,
-    ITEMS_CRAFTED_1 {
-      public final boolean qualifies(PlayerObject var1) {
-        return false;
-      }
-    },
-    ITEMS_CRAFTED_2 {
-      public final boolean qualifies(PlayerObject var1) {
-        return false;
-      }
-    },
-    ITEMS_CRAFTED_3 {
-      public final boolean qualifies(PlayerObject var1) {
-        return false;
-      }
-    },
-    ITEMS_CRAFTED_4 {
-      public final boolean qualifies(PlayerObject var1) {
-        return false;
-      }
-    },
-    SWORDS_OWNED_1 {
-      public final boolean qualifies(PlayerObject var1) {
-        return false;
-      }
-    },
-    SWORDS_OWNED_2 {
-      public final boolean qualifies(PlayerObject var1) {
-        return false;
-      }
-    },
-    SWORDS_OWNED_3 {
-      public final boolean qualifies(PlayerObject var1) {
-        return false;
-      }
-    },
-    HANDGUNS_OWNED_1 {
-      public final boolean qualifies(PlayerObject var1) {
-        return false;
-      }
-    },
-    HANDGUNS_OWNED_2 {
-      public final boolean qualifies(PlayerObject var1) {
-        return false;
-      }
-    },
-    HANDGUNS_OWNED_3 {
-      public final boolean qualifies(PlayerObject var1) {
-        return false;
-      }
-    },
-    BOMBS_OWNED_1 {
-      public final boolean qualifies(PlayerObject var1) {
-        return false;
-      }
-    },
-    BOMBS_OWNED_2 {
-      public final boolean qualifies(PlayerObject var1) {
-        return false;
-      }
-    },
-    BOMBS_OWNED_3 {
-      public final boolean qualifies(PlayerObject var1) {
-        return false;
-      }
-    },
-    HELMS_OWNED_1 {
-      public final boolean qualifies(PlayerObject var1) {
-        return false;
-      }
-    },
-    HELMS_OWNED_2 {
-      public final boolean qualifies(PlayerObject var1) {
-        return false;
-      }
-    },
-    HELMS_OWNED_3 {
-      public final boolean qualifies(PlayerObject var1) {
-        return false;
-      }
-    },
-    ARMORS_OWNED_1 {
-      public final boolean qualifies(PlayerObject var1) {
-        return false;
-      }
-    },
-    ARMORS_OWNED_2 {
-      public final boolean qualifies(PlayerObject var1) {
-        return false;
-      }
-    },
-    ARMORS_OWNED_3 {
-      public final boolean qualifies(PlayerObject var1) {
-        return false;
-      }
-    },
-    SHIELDS_OWNED_1 {
-      public final boolean qualifies(PlayerObject var1) {
-        return false;
-      }
-    },
-    SHIELDS_OWNED_2 {
-      public final boolean qualifies(PlayerObject var1) {
-        return false;
-      }
-    },
-    SHIELDS_OWNED_3 {
-      public final boolean qualifies(PlayerObject var1) {
-        return false;
-      }
-    },
-    RARITY_SET_1 {
-      public final boolean qualifies(PlayerObject var1) {
-        return false;
-      }
-    },
+    ITEMS_CRAFTED_1,
+    ITEMS_CRAFTED_2,
+    ITEMS_CRAFTED_3,
+    ITEMS_CRAFTED_4,
+    SWORDS_OWNED_1,
+    SWORDS_OWNED_2,
+    SWORDS_OWNED_3,
+    HANDGUNS_OWNED_1,
+    HANDGUNS_OWNED_2,
+    HANDGUNS_OWNED_3,
+    BOMBS_OWNED_1,
+    BOMBS_OWNED_2,
+    BOMBS_OWNED_3,
+    HELMS_OWNED_1,
+    HELMS_OWNED_2,
+    HELMS_OWNED_3,
+    ARMORS_OWNED_1,
+    ARMORS_OWNED_2,
+    ARMORS_OWNED_3,
+    SHIELDS_OWNED_1,
+    SHIELDS_OWNED_2,
+    SHIELDS_OWNED_3,
+    RARITY_SET_1,
     LEVELLED_ITEM_1,
     @Deprecated
     MINERALS_DEPOSITED_1,
@@ -177,25 +58,13 @@ public class Achievement {
     MINERALS_DEPOSITED_4,
     @Deprecated
     MINERALS_DEPOSITED_5,
-    SLOTS_FILLED_1 {
-      public final boolean qualifies(PlayerObject var1) {
-        return false;
-      }
-    },
+    SLOTS_FILLED_1,
     REVIVE_SELF,
     REVIVE_FRIEND,
     @Deprecated
     REVIVE_FRIEND_MINIMUM,
-    HEALTH_CAPSULES_USED_1 {
-      public final boolean qualifies(PlayerObject var1) {
-        return false;
-      }
-    },
-    REMEDY_CAPSULES_USED_1 {
-      public final boolean qualifies(PlayerObject var1) {
-        return false;
-      }
-    },
+    HEALTH_CAPSULES_USED_1,
+    REMEDY_CAPSULES_USED_1,
     MECHA_KNIGHT_ACTIVATED,
     REVIVELESS_COMPLETE_TIER_1,
     REVIVELESS_COMPLETE_TIER_2,
@@ -204,92 +73,11 @@ public class Achievement {
     REVIVELESS_FULL_RUN,
     FIRECRACKER_BOMB_OWNED,
     LOCKDOWN_SNOWBALL,
-    CASINO_SPIN_1 {
-      public final boolean qualifies(PlayerObject var1) {
-        return false;
-      }
-
-      public final boolean isHidden() {
-        return true;
-      }
-    },
-    CASINO_WHAMMIE {
-      public final boolean isHidden() {
-        return true;
-      }
-    },
-    CASINO_SPIN_2 {
-      public final boolean qualifies(PlayerObject var1) {
-        return false;
-      }
-
-      public final boolean isHidden() {
-        return true;
-      }
-    },
-    CASINO_SPIN_3 {
-      public final boolean qualifies(PlayerObject var1) {
-        return false;
-      }
-
-      public final boolean isHidden() {
-        return true;
-      }
-    },
-    CASINO_VIP {
-      public final boolean isHidden() {
-        return true;
-      }
-    },
-    UNUSED;
-
-    protected final int _code;
-
-    public boolean qualifies(PlayerObject var1) {
-      return false;
-    }
-
-    public boolean isHidden() {
-      return this == UNUSED || this.isObsolete();
-    }
-
-    public boolean isOnSteam() {
-      return !this.isHidden();
-    }
-
-    public boolean isObsolete() {
-      switch (this) {
-        case MINERALS_DEPOSITED_1:
-        case MINERALS_DEPOSITED_2:
-        case MINERALS_DEPOSITED_3:
-        case MINERALS_DEPOSITED_4:
-        case MINERALS_DEPOSITED_5:
-        case REVIVE_FRIEND_MINIMUM:
-          return true;
-        default:
-          return false;
-      }
-    }
-
-    public int code() {
-      return this._code;
-    }
-
-    public String key() {
-      return "m.achievement_" + Integer.toHexString(this._code);
-    }
-
-    public String description() {
-      String var1 = "d.achievement_" + Integer.toHexString(this._code);
-      return this.isObsolete() ? L.c("m.newline", new String[]{var1, "m.achievement_obsolete"}) : var1;
-    }
-
-    public String icon() {
-      return "ui/icon/cheevos/" + Integer.toHexString(this._code) + ".png";
-    }
-
-    private Type() {
-      this._code = Achievement.b(this);
-    }
+    CASINO_SPIN_1,
+    CASINO_WHAMMIE,
+    CASINO_SPIN_2,
+    CASINO_SPIN_3,
+    CASINO_VIP,
+    UNUSED
   }
 }
