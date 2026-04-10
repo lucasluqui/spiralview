424b8d2e4a28deb287f58f8e7d2c4acf2a79e95f
Lucas <lucasallegri@protonmail.com>
2026-04-10 17:37:45 -0300

    Back to development.

--- src/main/java/com/lucasluqui/spiralview/Globals.java
+++ src/main/java/com/lucasluqui/spiralview/Globals.java
@@ -3,6 +3,6 @@ package com.lucasluqui.spiralview;
 public class Globals
 {
   // TODO: Get this from Deploy/Build config classes.
-  public static final String VERSION = "2.1.3";
+  public static final String VERSION = "2.1.4-SNAPSHOT";
   public static final String USER_DIR = System.getProperty("user.dir");
 }
